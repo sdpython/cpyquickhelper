ac06cacc8636bf1804fd275c606f5adc67e5ee84
xavier dupré <xavier.dupre@gmail.com>
2017-12-09 21:47:57 +0100

    updaye build

--- setup.py
+++ setup.py
@@ -231,7 +231,7 @@ if not r:
                                              os.path.join(root, 'src/cpyquickhelper/parallel/win32')],
                                libraries=['pthreadVC2'])
     else:
-        extra_compile_args = ['-std=c++11', '-pthread']
+        extra_compile_args = None #['-lpthread']
         ext_thread = Extension('src.cpyquickhelper.parallel.threader',
                                [os.path.join(root, 'src/cpyquickhelper/parallel/threaderc.cpp'),
                                 os.path.join(root, 'src/cpyquickhelper/parallel/threader.cpp')],
