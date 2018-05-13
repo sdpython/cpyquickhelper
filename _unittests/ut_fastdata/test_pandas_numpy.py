d528973724d049a69fb6371e67c06fa84507a15d
xavier dupré <xavier.dupre@gmail.com>
2018-05-13 18:14:45 +0200

    update for pylint

--- _unittests/ut_fastdata/test_pandas_numpy.py
+++ _unittests/ut_fastdata/test_pandas_numpy.py
@@ -8,23 +8,9 @@ import os
 import unittest
 import numpy
 import pandas
+from pyquickhelper.loghelper import fLOG
+from pyquickhelper.pycode import ExtTestCase
 
-
-try:
-    import pyquickhelper as skip_
-except ImportError:
-    path = os.path.normpath(
-        os.path.abspath(
-            os.path.join(
-                os.path.split(__file__)[0],
-                "..",
-                "..",
-                "..",
-                "pyquickhelper",
-                "src")))
-    if path not in sys.path:
-        sys.path.append(path)
-    import pyquickhelper as skip_
 
 try:
     import src
@@ -39,8 +25,6 @@ except ImportError:
         sys.path.append(path)
     import src
 
-from pyquickhelper.loghelper import fLOG
-from pyquickhelper.pycode import ExtTestCase
 from src.cpyquickhelper.fastdata import df2array, df2arrays
 
 
