444bbbac327e4642bb537033b8e7e6117c70c598
xavier dupré <xavier.dupre@gmail.com>
2019-07-11 00:02:39 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -331,7 +331,7 @@ if not r:
         packages=packages,
         package_dir=package_dir,
         package_data=package_data,
-        setup_requires=["pybind11", "cython"],
+        setup_requires=["pybind11", "cython", "scipy"],
         install_requires=["pybind11", "numpy>=1.16",
                           "cython", 'scipy'],
     )
