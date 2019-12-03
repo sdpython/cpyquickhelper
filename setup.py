ac3673df1979516c98d5975e51bb8fac276d97a4
xavier dupré <xavier.dupre@gmail.com>
2019-12-03 01:07:09 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -203,9 +203,10 @@ if not r:
     elif sys.platform.startswith("darwin"):
         libraries_thread = None
         extra_compile_args_thread = ['-lpthread', '-stdlib=libc++', '-std=c++11',
-                                     '-mmacosx-version-min=10.7', '-fopenmp']
+                                     '-mmacosx-version-min=10.7', '-Xpreprocessor',
+                                     '-fopenmp']
         extra_compile_args_numbers = ['-stdlib=libc++', '-mmacosx-version-min=10.7',
-                                      '-std=c++11', '-fopenmp']
+                                      '-std=c++11', '-Xpreprocessor', '-fopenmp']
         extra_compile_args_bench = extra_compile_args_numbers.copy()
         extra_compile_args_bench.append('-fpermissive')
         extra_link_args = ["-lomp"]
