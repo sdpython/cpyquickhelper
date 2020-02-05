7b899010f1f1e1ed3a954664f5b31341e8faf2ef
xavier dupré <xavier.dupre@gmail.com>
2020-02-05 23:23:45 +0100

    documentation

--- _doc/sphinxdoc/source/conf.py
+++ _doc/sphinxdoc/source/conf.py
@@ -9,7 +9,7 @@ local_template = os.path.join(os.path.abspath(
     os.path.dirname(__file__)), "phdoc_templates")
 
 from pyquickhelper.helpgen.default_conf import set_sphinx_variables, get_default_stylesheet
-set_sphinx_variables(__file__, "cpyquickhelper", "Xavier Dupré", 2019,
+set_sphinx_variables(__file__, "cpyquickhelper", "Xavier Dupré", 2020,
                      "readable", sphinx_readable_theme.get_html_theme_path(),
                      locals(), extlinks=dict(
                          issue=('https://github.com/sdpython/cpyquickhelper/issues/%s', 'issue')),
