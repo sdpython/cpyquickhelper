83b35fd7b585ad60eb4075b3db0ea26da65355db
xavier dupré <xavier.dupre@gmail.com>
2020-06-22 10:28:28 +0200

    documentation

--- _doc/sphinxdoc/source/conf.py
+++ _doc/sphinxdoc/source/conf.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import sys
 import os
-import sphinx_readable_theme
+import alabaster
 
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.split(__file__)[0])))
@@ -10,7 +10,7 @@ local_template = os.path.join(os.path.abspath(
 
 from pyquickhelper.helpgen.default_conf import set_sphinx_variables, get_default_stylesheet
 set_sphinx_variables(__file__, "cpyquickhelper", "Xavier Dupré", 2020,
-                     "readable", sphinx_readable_theme.get_html_theme_path(),
+                     "alabaster", alabaster.get_path(),
                      locals(), extlinks=dict(
                          issue=('https://github.com/sdpython/cpyquickhelper/issues/%s', 'issue')),
                      title="cpyquickhelper", book=True)
