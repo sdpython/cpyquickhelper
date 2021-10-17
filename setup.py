50198e9209e2c3f91a2f549f579eb10f8c870e0a
xavier dupré <xavier.dupre@gmail.com>
2021-10-17 23:04:51 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -231,7 +231,7 @@ def get_extensions():
         extra_compile_args=["-O3"],
         define_macros=define_macros)
 
-    ext_numbers = Extension(
+    ext_profiling = Extension(
         'cpyquickhelper.profiling._event_profiler',
         [os.path.join(root, 'cpyquickhelper/profiling/_event_profiler.cpp'),
          os.path.join(root, 'cpyquickhelper/profiling/_event_profiler_python.cpp')],
@@ -270,7 +270,8 @@ def get_extensions():
             ext_thread, ext_stdhelper,
             ext_numbers, ext_benchmark,
             ext_benchmark_dot,
-            ext_benchmark_sum_type
+            ext_benchmark_sum_type,
+            ext_profiling
         ])
     return ext_modules
 
