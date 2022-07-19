cdfebf00c86d071d92f1ec677e0ef3e6887a25c3
xavier dupré <xavier.dupre@gmail.com>
2022-07-20 00:54:22 +0200

    Update _event_profiler_python.cpp

--- cpyquickhelper/profiling/_event_profiler_python.cpp
+++ cpyquickhelper/profiling/_event_profiler_python.cpp
@@ -3,7 +3,7 @@
 
 #ifndef SKIP_PYTHON
 #include <pybind11/pybind11.h>
-#include <pybind11/operators.h>
+//#include <pybind11/operators.h>
 #include <pybind11/stl.h>
 #include <pybind11/numpy.h>
 #include <pybind11/functional.h>
