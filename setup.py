e020c332b58203df4721ad4f89c8e76d4e32d910
xavier dupré <xavier.dupre@gmail.com>
2022-07-20 01:48:24 +0200

    update pybind11 version

--- setup.py
+++ setup.py
@@ -379,7 +379,7 @@ setup(
     packages=packages,
     package_dir=package_dir,
     package_data=package_data,
-    setup_requires=["pybind11", "cython", "scipy", "numpy", "pyquicksetup"],
-    install_requires=["pybind11", "numpy>=1.16",
+    setup_requires=["pybind11<2.10.0", "cython", "scipy", "numpy", "pyquicksetup"],
+    install_requires=["pybind11<2.10.0", "numpy>=1.16",
                       "cython", 'scipy', 'pandas>=1.0'],
 )
